fa05b73c6181b428876d109c8013e2f57d32757e
V Sreekanth <sreeix@gmail.com>
2013-09-18 23:31:24 +0530

    More repl fixes. To Set it inside and with hull

--- repl/repl.js
+++ repl/repl.js
@@ -1,3 +1,7 @@
 var arDrone = require('ar-drone');
 var client  = arDrone.createClient();
+client.ftrim();
+client.config('general:navdata_data_demo', false);
+client.config('control:outdoor', false); // This is not outdoor. 
+client.config('control:flight_without_shell', false); // Using the shell all the time
 client.createRepl();
